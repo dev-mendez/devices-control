e89baebbb299947852051023aee17dbd18407a05
An2 <an2m001@gmail.com>
2023-06-12 15:37:38 -0400

    chore: generate and clean project structure

---
+++ server/src/master_device/schemas/masterdevice.schema.ts
@@ -0,0 +1,38 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Peripheral } from './peripheral.schema';
+import mongoose from 'mongoose';
+
+export type MasterDeviceDocument = MasterDevice & Document;
+
+@Schema({
+  timestamps: true,
+})
+export class MasterDevice {
+  @Prop({
+    unique: true,
+    trim: true,
+    required: true,
+  })
+  serialNumber: string;
+
+  @Prop({
+    trim: true,
+    required: true,
+  })
+  name: string;
+
+  @Prop({
+    unique: true,
+    trim: true,
+    required: true,
+  })
+  ipV4: string;
+
+  @Prop([{ type: mongoose.Schema.Types.ObjectId, ref: 'Peripheral' }])
+  devices: [Peripheral];
+
+  @Prop({ default: false })
+  isDeleted: boolean;
+}
+
+export const MasterDeviceSchema = SchemaFactory.createForClass(MasterDevice);
---
+++ server/src/master_device/schemas/peripheral.schema.ts
@@ -0,0 +1,26 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+
+export type PeripheralDocument = Peripheral & Document;
+
+@Schema({ timestamps: true })
+export class Peripheral {
+  @Prop({ required: true, index: true })
+  uid: number;
+
+  @Prop({ required: true })
+  vendor: string;
+
+  @Prop({ default: false })
+  status: boolean;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+  })
+  idGateway: string;
+
+  @Prop({ default: false })
+  isDeleted: boolean;
+}
+
+export const PeripheralSchema = SchemaFactory.createForClass(Peripheral);
