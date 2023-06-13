4bcc4192e21d298a846a4150ad2d39e91f9761e7
An2 <an2m001@gmail.com>
2023-06-13 14:58:27 -0400

    feat: connect db with mongo and docker

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { MasterDeviceModule } from './master_device/masterdevice.module';
 
 @Module({
-  imports: [MasterDeviceModule],
+  imports: [
+    MasterDeviceModule,
+    MongooseModule.forRoot(
+      'mongodb://admin:123@localhost:27017/devices-control?authSource=admin',
+    ),
+  ],
 })
 export class AppModule {}
